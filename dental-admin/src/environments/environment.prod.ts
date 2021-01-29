645584a26b24c96aebfea8d62a0d00aeef97abd6
Marlon <mdcasusol@gmail.com>
2021-01-29 22:47:56 +0100

    cambio app 202101292247

--- dental-admin/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  ENDPOINT_API : 'http://localhost:3000'
-};
--- dental-api/routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    require('./usuario')(app);
-    require('./login')(app);
-    require('./doctor')(app);
-    require('./especialidad')(app);
-}
