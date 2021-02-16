01916ccc6920cb7ea013eafeb105703e60e56afe
Marlon <mdcasusol@gmail.com>
2021-02-16 19:49:02 +0100

    maj

--- Docker/pipeline_node/node/demoApi/app.js
+++ Docker/pipeline_node/node/demoApi/app.js
@@ -4,11 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Pour declarer les utils CORS 
+const cors = require('cors')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var voitureRouter = require('./routes/voiture');
 
 var app = express();
+
+// Pour utiliser CORS dans cette projet
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
